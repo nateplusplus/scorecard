741fa5d6476de943fde6884593394c151439ae6d
Nathan Blair <nateblairfilm@gmail.com>
2017-05-14 20:27:14 -0500

    Pulling data from mongoDB backend using nodejsgit add --all! I MAKE THINGS DO STUFFgit add --all

--- src/index.js
+++ src/index.js
@@ -22,7 +22,46 @@ var PLAYERS = [
 	},
 ];
 
-ReactDOM.render(
-	<App initialPlayers={PLAYERS} title='Simple Scoreboard' />,
-	document.getElementById('root')
-);
+var defaultTitle = "Simple Scoreboard";
+
+var http = require('http');
+var req = http.get('http://localhost:55414', (res) => {
+	const statusCode = res.statusCode;
+	const contentType = res.headers['content-type'];
+
+	let error;
+	if (statusCode !== 200) {
+		error = new Error(`Request Failed.\n` +
+											`Status Code: ${statusCode}`);
+	} else if (!/^application\/json/.test(contentType)) {
+		error = new Error(`Invalid content-type.\n` +
+						`Expected application/json but received ${contentType}`);
+	}
+	if (error) {
+		console.log(error.message);
+		// consume response data to free up memory
+		res.resume();
+		return;
+	}
+
+	res.setEncoding('utf8');
+	let rawData = '';
+	res.on('data', (chunk) => rawData += chunk);
+	res.on('end', () => {
+		try {
+			let parsedData = JSON.parse(rawData);
+			defaultTitle = parsedData[0].title;
+
+			ReactDOM.render(
+				<App initialPlayers={PLAYERS} title={defaultTitle} />,
+				document.getElementById('root')
+			);
+		} catch (e) {
+			console.log(e.message);
+		}
+	});
+});
+
+req.on('error', (e) => {
+	console.log(`problem with request: ${e}`);
+});
